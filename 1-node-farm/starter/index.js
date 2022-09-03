7133da4bb33768908076f7efd53fa1d77156b06f
BOBCATSON <robjcatlin@gmail.com>
2022-09-03 12:25:49 +0100

    completed node farm code

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+// don't need to add the .js suffix
+const replaceTemplate = require('./modules/replaceTemplate');
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know about the Avocado ${textIn}. \nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written');
+
+// Non-Blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('Error! 🤯');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your files have been truncated 🥰');
+//       })
+//     });
+//   });
+// });
+
+// console.log('last console log right here guys..');
+
+
+// readFileSync is blocking, but it's ok to use if it is called once before the app is started?
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////// SERVER
+const server = http.createServer((req, res) => {
+
+  const { query, pathname } = url.parse(req.url, true);
+
+
+
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+
+    // loop over the dataObj array which holds all the products, then each iteration we will replace the {%PLACEHOLDERS%} in the template-card.html 
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    // we dont need to wrap the placeholder in a regex as there is only one element to be found and replaced
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    
+    res.end(output);
+
+
+
+
+  // Product page  
+  } else if (pathname === '/product') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+
+
+  // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, {'Content-type': 'application/json'});
+    res.end(data);
+
+
+
+
+
+  // Not Found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'My-own-header': 'hello-world',
+    });
+    res.end('<h1>This page doesn\'t exist</h1>');
+  }
+
+});
+
+// Server - 127.0.0.1:8000;
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
