0a1152b232cba7fcaa8c2d83414ea9ac03ad356a
BOBCATSON <robjcatlin@gmail.com>
2022-09-05 00:37:01 +0100

    set up Prettier extension

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-// JS required modules 
+// JS required modules
 // don't need to add the .js suffix
 const replaceTemplate = require('./modules/replaceTemplate');
 // NPM modules
@@ -31,67 +31,63 @@ const slugify = require('slugify');
 
 // console.log('last console log right here guys..');
 
-
 // readFileSync is blocking, but it's ok to use if it is called once before the app is started?
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map(el => slugify(el.productName, { lower: true } ));
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////// SERVER
 const server = http.createServer((req, res) => {
-
   const { query, pathname } = url.parse(req.url, true);
-
-
-
 
   // Overview page
   if (pathname === '/' || pathname === '/overview') {
-    res.writeHead(200, {'Content-type': 'text/html'});
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-    // loop over the dataObj array which holds all the products, then each iteration we will replace the {%PLACEHOLDERS%} in the template-card.html 
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    // loop over the dataObj array which holds all the products, then each iteration we will replace the {%PLACEHOLDERS%} in the template-card.html
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
     // we dont need to wrap the placeholder in a regex as there is only one element to be found and replaced
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-    
+
     res.end(output);
 
-
-
-
-  // Product page  
+    // Product page
   } else if (pathname === '/product') {
-    res.writeHead(200, {'Content-type': 'text/html'});
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
 
-
-
-  // API
+    // API
   } else if (pathname === '/api') {
-    res.writeHead(200, {'Content-type': 'application/json'});
+    res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
 
-
-
-
-
-  // Not Found
+    // Not Found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
       'My-own-header': 'hello-world',
     });
-    res.end('<h1>This page doesn\'t exist</h1>');
+    res.end("<h1>This page doesn't exist</h1>");
   }
-
 });
 
 // Server - 127.0.0.1:8000;
