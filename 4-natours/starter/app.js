d8ebfdd6d0e876bd543189d587011a2ea9a4b4fc
BOBCATSON <robjcatlin@gmail.com>
2022-09-14 22:10:58 +0100

    added package.json/package-lock.json to correct folder

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res
@@ -11,8 +15,26 @@ const app = express();
 //   res.send('You can post to this endpoint');
 // });
 
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
 app.get('/api/v1/tours', (req, res) => {
-  res.send('<h1>Hello, Route!</h1>');
+  res.status(200).json({
+    // Jsend Standard format
+    status: 'success',
+    // Shows number of results that are being sent
+    results: tours.length,
+    data: {
+      // json file is called tours-simple.json, so is the route /tours
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  res.send('Done!');
 });
 
 const port = 3000;
