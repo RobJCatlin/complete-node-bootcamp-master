bd0638144468c06a8d481f8182dec4eec675135a
BOBCATSON <robjcatlin@gmail.com>
2022-11-01 10:06:49 +0000

    setting up MongoDB, mongoose, set up data model

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,18 +1,26 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {});
+  });
+};
+
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
         if (err) return console.log(err.message);
         console.log('logged random dog img to dog-img.txt');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,54 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// MongoClient constructor
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(`ERROR 🚨:`, err);
+  });
 
 // console.log(process.env);
 
